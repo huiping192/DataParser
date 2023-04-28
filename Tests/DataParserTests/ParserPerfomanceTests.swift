81270e4db8f82dfaeddc3ec148a831db5f463ca2
Huiping Guo <huiping192@gmail.com>
2023-04-29 00:48:48 +0900

    fix performance test

--- Tests/DataParserTests/ParserPerfomanceTests.swift
+++ Tests/DataParserTests/ParserPerfomanceTests.swift
@@ -3,53 +3,53 @@ import XCTest
 
 final class ParserPerfomanceTests: XCTestCase {
   
-  let testCounts = 30000
+  let testCounts = 1000000
   
-  func testParserPerformanceC() {
-    let testData = generateTestData()
+  func testPerformanceComparison() {
+    let cPerformance = testParserPerformanceC()
+    let swiftPerformance = testParserPerformanceSwift()
     
+    let percentageDifference = ((swiftPerformance / cPerformance) - 1) * 100
+    print("C time: \(cPerformance) seconds, Swift time: \(swiftPerformance) seconds")
+    print("C is better by \(percentageDifference)%")
+  }
+  
+  func measurePerformance(_ block: () -> Void) -> TimeInterval {
+    let startTime = Date()
+    block()
+    let endTime = Date()
+    
+    return endTime.timeIntervalSince(startTime)
+  }
+  func testParserPerformanceC() -> TimeInterval {
     let parser = DataParser(start: "<data>", end: "</data>")
     
-    self.measure {
-      parser.appendData(testData)
-      var count = 0
-      
-      while let _ = parser.processData() {
-        count += 1
+    return measurePerformance {
+      for _ in 0 ..< testCounts {
+        parser.appendData(generateTestData)
+        _ = parser.processData()
       }
-      
-      XCTAssertEqual(count, testCounts, "Processed data count should be \(testCounts)")
     }
   }
   
-  func testParserPerformanceSwift() {
-    let testData = generateTestData()
-    
+  func testParserPerformanceSwift() -> TimeInterval {
     let parser = DataParserSwift(start: "<data>", end: "</data>")
     
-    self.measure {
-      parser.appendData(testData)
-      var count = 0
-      
-      while let _ = parser.processData() {
-        count += 1
+    return measurePerformance {
+      for _ in 0 ..< testCounts {
+        parser.appendData(generateTestData)
+        _ = parser.processData()
       }
-      
-      XCTAssertEqual(count, testCounts, "Processed data count should be \(testCounts)")
     }
   }
   
-  private func generateTestData() -> Data {
+  private var generateTestData: Data = {
     var data = "Some random data\n"
     let item = "<data>sample data</data>"
-    
-    for _ in 0..<testCounts {
-      data += "\(item)\n"
-    }
-    
+    data += "\(item)\n"
     data += "More random data"
     
     return data.data(using: .utf8)!
-  }
+  }()
   
 }
